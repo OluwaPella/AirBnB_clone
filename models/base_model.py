263bf96b9aa9ed287d3683396c20a53b7a000e59
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-08-13 16:24:10 -0700

    model

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
 
-class BaseModel():
-      """
-    Base class that defines common attributes and methods for other classes"""
-    def __int__ = (self, *arg, **kwargs):
-        """*arg and **kwargs are Keyword arguments used to create the instance attributes"""
+
+class BaseModel:
+    """Base class that defines common attributes and
+    methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """*args and **kwargs are Keyword arguments
+        used to create the instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-       
-       if kwargs:
-           for key, value in kwargs.item():
-               if key == 'created_at' or 'updated_at':
-                   setattr(self, key , datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-               else
-               self.id = str(uuid.uuid4())
-               self.created_at = self.updated_at = datetime.now()
-               def __str__ = (self):
-                   """Returns a string representation of the instance"""
-                   return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-               def save(self):
-                   """updateing the public instance attribute updated_at with the current datetime"""
-                   self.updated_at = datetime.now()
-                   to_dict(self):
-                        """Converts the instance attributes to a dictionary representation"""
-                      ob_dict = self.__dict__.copy()
-                      ob_dict['__class__'] = self.__class__.__name_
-                      ob_dict['created_at'] = self.created_at.isoformat()
-                      ob_dict['updated_at'] = seld.updated_at.isoformat()
-                      return ob_dict
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime
+                            (value, '%Y-%m-%dT%H:%M:%S.%f'))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"{self.__class__.__name__}
+    - ID: {self.id}\nAttributes: {self.__dict__}"
+
+    def save(self):
+        """Updating the public instance attribute
+        updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Converts the instance attributes to a dictionary representation"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
